1b28d1b2e830dc6789a7e8aa0c9149917f2964b6
cesar-infante <cesar261110@gmail.com>
2022-06-01 18:43:25 -0700

    corrections to js

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 // * As a user, I want to play Rock, Paper, Scissors against an automated opponent.
 let userChoice = document.getElementById('textBox');
-function G() {
-  console.log('Cesar Is Awesome')
-}
-G();
-
 
 
 // * As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
 
 
-
 // * As a user, I expect the computer to choose R, P, or S in return.
-function computer()
 
-console.log("Hello World!")
-console.log("Nate The Great")
-  
+
 // * As a user, I want the option to play again whether I win or lose.
-
 
 
 // * As a user, I want to see my total wins, ties, and losses after each round.
