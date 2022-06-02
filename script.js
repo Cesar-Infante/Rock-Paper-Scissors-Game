2bf8a86ab777f8d0985fd17797844b8f3482ceef
cesar-infante <cesar261110@gmail.com>
2022-06-01 18:03:59 -0700

    my line of code

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ function Name() {
 
 
 // * As a user, I expect the computer to choose R, P, or S in return.
-
-
-
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
+  
+  addThreeNumbers(1, 2, 3, 4);
+  console.log(addThreeNumbers(1, 2, 3, 4));
+  
 // * As a user, I want the option to play again whether I win or lose.
 
 
