840f1b66d22ba76b5ac24b92390658bc9f4b0c82
Cesar's PC <cesar261110@gmaill.com>
2022-11-07 14:32:57 -0800

    Practicing from JavaScript class, RPS game, logic for first half created

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 // * As a user, I want to play Rock, Paper, Scissors against an automated opponent.
-function Name() {
-  console.log('Hello')
+let name;
+let usersChoice;
+function startGame() {
+  // prompt the user for their name and store that value
+  name = window.prompt('What is your name?');
+  if (name === null) {
+    return;
+  } else {
+    getChoice();
+  }
+};
+startGame();
+
+function getChoice() {
+  // get the Users choice of either R,P,S OR rock, paper, scissors
+  usersChoice = window.prompt(`${name}, Please type either R, P, S or rock, paper, scissors`);
+  console.log(usersChoice)
+  checkChoice();
 }
 
+function checkChoice() {
+  // check to see if the user input either r,p,s or rock, paper, scissors
+  if (usersChoice === null) {
+    return;
+  } else if (usersChoice === 'R' || usersChoice === 'r' || usersChoice === "Rock" || usersChoice === 'rock') {
+    computerChoice();
+  } else if (usersChoice === 'P' || usersChoice === 'p' || usersChoice === "Paper" || usersChoice === 'paper') {
+    computerChoice();
+  } else if (usersChoice === 'S' || usersChoice === 's' || usersChoice === "Scissors" || usersChoice === 'scissors') {
+    computerChoice();
+  } else {
+    window.alert('Error: Wrong choice selected');
+    getChoice();
+  }
+}
+
+function computerChoice() {
+  window.alert(`Hello World`)
+}
 
 
 // * As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
